9c3a1797256089bd72ce407fd71b6f3b0baa944c
Jamesson Junior <jamesson.junior@capco.com>
2022-12-26 14:59:56 -0300

    Created shop-validator

---
+++ shop-validator/src/main/java/br/com/jamesson/shopvalidator/ShopValidatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamesson.shopvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopValidatorApplication.class, args);
+	}
+
+}
