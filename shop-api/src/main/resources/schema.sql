ab1a072db2243de94242245c56c6c676e6bf0937
Jamesson Junior <jamesson.junior@capco.com>
2022-12-26 14:07:40 -0300

    Adjust in sql

--- shop-api/src/main/resources/schema.sql
+++ shop-api/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table shop (
     id bigint primary key auto_increment,
-    buyer_identifier varchar(100) not null,
+--    buyer_identifier varchar(100) not null,
     identifier varchar not null,
     status varchar not null,
     date_shop date
