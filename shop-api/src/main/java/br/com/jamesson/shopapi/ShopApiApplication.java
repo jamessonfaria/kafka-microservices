f3a7c92ffd94f5094c30c97920b1aac10f05643e
Jamesson Junior <jamesson.junior@capco.com>
2022-12-26 11:41:12 -0300

    Initial commit

---
+++ shop-api/src/main/java/br/com/jamesson/shopapi/ShopApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamesson.shopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApiApplication.class, args);
+	}
+
+}
---
+++ shop-api/src/test/java/br/com/jamesson/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamesson.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
