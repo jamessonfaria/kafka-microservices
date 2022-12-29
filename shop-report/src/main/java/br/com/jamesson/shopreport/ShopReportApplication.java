b3779130a8ea93bb7e9682ddeddc9cf028f88d6b
Jamesson Junior <jamesson.junior@capco.com>
2022-12-29 10:25:32 -0300

    Create project shop-report

---
+++ shop-report/src/main/java/br/com/jamesson/shopreport/ShopReportApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamesson.shopreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopReportApplication.class, args);
+	}
+
+}
---
+++ shop-report/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table shop_report (
+    id bigint primary key auto_increment,
+    identifier varchar(100) not null,
+    amount int not null
+);
