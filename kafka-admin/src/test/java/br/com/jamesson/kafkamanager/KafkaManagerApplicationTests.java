9a32ead7e7f949666cace10e98925c3083a6c49f
Jamesson Junior <jamesson.junior@capco.com>
2023-01-02 15:02:42 -0300

    Create projet kafka admin

---
+++ kafka-admin/src/test/java/br/com/jamesson/kafkamanager/KafkaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamesson.kafkamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
